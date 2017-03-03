d1bbd3a1837e49910aa8fb52e17fb59f5495a790
Aurélien Picolet <aurelien.picolet@gmail.com>
2017-03-04 00:38:22 +0100

    beginning of audio
    creating own audio manager?
    trying stuff

---
+++ Emission_Unity/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	
+	public AudioClip[] audioClipArray;
+	private Dictionary<string, AudioClip> audioClipDic = new Dictionary<string, AudioClip>();
+
+	void Awake() {
+		foreach(AudioClip ac in audioClipArray) {
+			audioClipDic.Add(ac.name, ac);
+		}
+		audioClipArray = null;
+	}
+
+	// Use this for initialization
+//	void Start () {
+//	}
+
+	public void PlayAudioClip(string name, float volume = 1.0f , Vector3 worldPosition = default(Vector3)) {
+		AudioClip ac;
+		if(audioClipDic.TryGetValue(name, out ac)) {
+			AudioSource.PlayClipAtPoint(ac, worldPosition, volume);
+		}
+	}
+
+	public void PlayAudioClip(AudioClip audioClip, float volume = 1.0f, Vector3 worldPosition = default(Vector3)) {
+		AudioSource.PlayClipAtPoint(audioClip, worldPosition, volume);
+	}
+
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+}
