8cacddc93875840aba275754e51625eb683a61b2
Aurélien Picolet <aurelien.picolet@gmail.com>
2017-03-07 17:32:24 +0100

    script possibility for camera shake
    but in the end not implementing it, maybe too much

---
+++ Emission_Unity/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraScript : MonoBehaviour {
+
+	private Sequence shakeAnimSeq;
+
+	void Awake() {
+		shakeAnimSeq = DOTween.Sequence();
+		shakeAnimSeq.SetAutoKill(false);
+		shakeAnimSeq.Append(transform.DOShakePosition(0.4f, 0.1f, 20, 90));
+	}
+
+	// Use this for initialization
+//	void Start () {
+//		
+//	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	public void CameraShake() {
+		shakeAnimSeq.Restart();
+	}
+}
